b75c693dd775b0f87732a312ce24234f87b6edc1
Niklas Marion <niklas.marion@gmx.de>
2025-06-01 20:27:26 +0200

    update list id

--- download.sh
+++ download.sh
@@ -7,10 +7,10 @@ python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type
 python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/EUR/category/out/age/senior --output records/europe.csv --code AR
 python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/OCR/category/out/age/senior --output records/oceania.csv --code AR
 
-python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/WR/list/1058/category/out --output leads/world.csv --code WL
-python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/AFR/list/1058/category/out --output leads/africa.csv --code AL
-python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/AMR/list/1058/category/out --output leads/america.csv --code AL
-python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/ASR/list/1058/category/out --output leads/asia.csv --code AL
-python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/EUR/list/1058/category/out --output leads/europe.csv --code AL
-python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/OCR/list/1058/category/out --output leads/oceania.csv --code AL
+python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/WR/list/1091/category/out --output leads/world.csv --code WL
+python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/AFR/list/1091/category/out --output leads/africa.csv --code AL
+python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/AMR/list/1091/category/out --output leads/america.csv --code AL
+python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/ASR/list/1091/category/out --output leads/asia.csv --code AL
+python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/EUR/list/1091/category/out --output leads/europe.csv --code AL
+python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/OCR/list/1091/category/out --output leads/oceania.csv --code AL
 python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/PR/category/out/age/senior --output records/paralympics.csv --code PR
