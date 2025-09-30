6c79a0fb5f39703711ec48077d514c23fc862943
Ferries-01 <ferries@ferries-timing.de>
2025-09-30 16:02:38 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/WR/category/out/age/senior --output records/world.csv 
-python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/AFR/category/out/age/senior --output records/africa.csv --code AR
-python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/AMR/category/out/age/senior --output records/america.csv --code AR
-python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/ASR/category/out/age/senior --output records/asia.csv --code AR
-python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/EUR/category/out/age/senior --output records/europe.csv --code AR
-python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/OCR/category/out/age/senior --output records/oceania.csv --code AR
+python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/AFR/category/out/age/senior --output records/africa.csv --code AF
+python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/AMR/category/out/age/senior --output records/america.csv --code AM
+python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/ASR/category/out/age/senior --output records/asia.csv --code AS
+python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/EUR/category/out/age/senior --output records/europe.csv --code ER
+python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/OCR/category/out/age/senior --output records/oceania.csv --code OC
 
 python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/WR/list/1091/category/out --output leads/world.csv --code WL
 python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/AFR/list/1091/category/out --output leads/africa.csv --code AL
@@ -14,3 +14,4 @@ python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/typ
 python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/EUR/list/1091/category/out --output leads/europe.csv --code AL
 python3 main.py --url https://www.ipc-services.org/sdms/web/ranking/at/excel/type/OCR/list/1091/category/out --output leads/oceania.csv --code AL
 python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/PR/category/out/age/senior --output records/paralympics.csv --code PR
+python3 main.py --url https://www.ipc-services.org/sdms/web/record/at/excel/type/CR/category/out/age/senior --output records/championship.csv --code CR
